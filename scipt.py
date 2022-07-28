82b17a1fc5d6c1f30bb9e3230629ba2dd56e2153
Mark Zhang <umzhang2@hotmail.com>
2022-07-27 21:24:21 -0500

    The 2nd commit

--- scipt.py
+++ scipt.py
@@ -13,5 +13,7 @@ def greet(who_to_greet):
     return greeting
 
 
-r = requests.get("https://github.com/umzhang2/travel-site")
+r = requests.get(
+    "https://prodentim.com/video.php?hop=boldapex&everclk=30bb32353b6749aaa17c820d888b8910&obclick=v1-171ce0781fff8ae5cad2cc09aa6a82a0-005922967abbb128b7724039d48393efc1-heygem3cg4yweljugqzdeljumm4tellcmq4ggljzgnrtkmbzmzrtcobvge"
+)
 print(r.status_code)
